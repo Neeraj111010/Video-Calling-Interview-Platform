acfdff1d6bdb1a7e45343cac1b4268cba5b7fa2c
K S Neeraj <ksneeraj131410@gmail.com>
2025-12-19 17:58:10 +0530

    Revert "Merge pull request #19 from Neeraj111010/problem-detail-branch"
    This reverts commit 3127da373350a90c4c04bb5679dc601e8d4cd246, reversing
    changes made to a592217e745b998cf1412c7fac4c4c65ce5b22da.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import HomePage from './pages/HomePage'
 import ProblemsPage from './pages/ProblemsPage'
 import { Toaster } from 'react-hot-toast'
 import DashBoardPage from './pages/DashBoardPage'
-import ProblemPage from './pages/ProblemPage'
 
 function App() {
     const {isSignedIn,isLoaded}=useUser()
@@ -15,7 +14,6 @@ function App() {
     <Route path='/' element={ !isSignedIn ? <HomePage/>: <Navigate to={"/dashboard"}/>}/>
     <Route path='/dashboard' element={ isSignedIn ? <DashBoardPage/>: <Navigate to={"/"}/>}/>
     <Route path='/problems' element={isSignedIn? <ProblemsPage/>: <Navigate to={"/"}/>}/>
-    <Route path='/problem/:id' element={isSignedIn? <ProblemPage/>: <Navigate to={"/"}/>}/>
     </Routes>
     <Toaster/>
     </>
