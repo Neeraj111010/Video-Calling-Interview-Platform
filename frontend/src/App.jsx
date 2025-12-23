5a203d7f860732ef353979b5ef3180f569526055
K S Neeraj <ksneeraj131410@gmail.com>
2025-12-24 01:19:02 +0530

    Refactor chat and session controllers, update stream client integration, and enhance middleware error handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser } from '@clerk/clerk-react'
 import { Navigate, Route, Routes } from 'react-router'
-import HomePage from './pages/HomePage'
-import ProblemsPage from './pages/ProblemsPage'
+import HomePage from './pages/HomePage.jsx'
+import ProblemsPage from './pages/ProblemsPage.jsx'
 import { Toaster } from 'react-hot-toast'
-import DashBoardPage from './pages/DashBoardPage'
-import ProblemPage from './pages/ProblemPage'
-import SessionPage from './pages/SessionPage'
+import DashBoardPage from './pages/DashBoardPage.jsx'
+import ProblemPage from './pages/ProblemPage.jsx'
+import SessionPage from './pages/SessionPage.jsx'
 
 function App() {
     const {isSignedIn,isLoaded}=useUser()
