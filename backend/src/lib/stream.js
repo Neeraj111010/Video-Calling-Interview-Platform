414e360acbaec1041b7d0bd01ca8dec7b7af775e
K S Neeraj <ksneeraj131410@gmail.com>
2025-12-24 01:59:47 +0530

    change done in inngest as well as stream

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,35 +1,31 @@
-import {StreamChat} from "stream-chat"
+import { StreamChat } from "stream-chat";
 import { StreamClient } from "@stream-io/node-sdk";
-import { ENV } from "./env.js"
+import { ENV } from "./env.js";
 
-const apiKey=ENV.STREAM_API_KEY
-const apiSecret=ENV.STREAM_API_SECRET
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
 
-if(!apiKey || !apiSecret){
-    console.error("STREAM_API_KEY or STREAM_API_SECRET is missing")
+if (!apiKey || !apiSecret) {
+  console.error("STREAM_API_KEY or STREAM_API_SECRET is missing");
 }
 
-export const streamClient=new StreamClient(apiKey,apiSecret) //will be used for video calls
-export const chatClient=StreamChat.getInstance(apiKey,apiSecret)// will be used for chat feature
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret); // will be used chat features
+export const streamClient = new StreamClient(apiKey, apiSecret); // will be used for video calls
 
-export const upsertStreamUser=async(userData)=>{
-    try {
-        await chatClient.upsertUser(userData)
-        console.log("Stream user upserted succcessfully",userData)
-        return userData
-    } catch (error) {
-        console.error("Error upserting stream user:",error)
-    }
-}
+export const upsertStreamUser = async (userData) => {
+  try {
+    await chatClient.upsertUser(userData);
+    console.log("Stream user upserted successfully:", userData);
+  } catch (error) {
+    console.error("Error upserting Stream user:", error);
+  }
+};
 
-
-export const deleteStreamUser=async(userId)=>{
-    try {
-        await chatClient.deleteUser(userId)
-        console.log("Stream user deleted succesfully:",userId)
-        return userData
-    } catch (error) {
-        console.error("Error deleting the stream user:",error)
-    }
-}
-
+export const deleteStreamUser = async (userId) => {
+  try {
+    await chatClient.deleteUser(userId);
+    console.log("Stream user deleted successfully:", userId);
+  } catch (error) {
+    console.error("Error deleting the Stream user:", error);
+  }
+};
